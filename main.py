4bf63868d6b681919a8d37da8a61c76da621f251
Gabriel San Martin <gssm.snf19@uea.edu.br>
2019-09-26 23:28:41 -0400

    Janela basica

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import turtle
 
-print("Hello, World")
+playing = True
+def close_screen():
+    global playing
+    playing = not playing
+
+def create_screen(title, width, height):
+    screen = turtle.Screen()
+    screen.title(title)
+    screen.bgcolor("black")
+    screen.setup(width=width, height=height)
+    screen.tracer(0)
+    return screen
+
+screen = create_screen("Breakout", 800, 600)
+root = screen.getcanvas().winfo_toplevel()
+root.protocol("WM_DELETE_WINDOW", close_screen)
+
+while playing:
+    screen.update()
