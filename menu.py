42d90c55f1aa86932a69865323b24dde667e3095
Vanessa Câmara <vdsc.snf19@uea.edu.br>
2019-10-05 14:57:09 -0400

    Adicionar título na tela

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 # criar tela
 from game_modules import objects
 screen = objects.create_screen("Breakout", 800, 600)
-
+hud_title = objects.create_hud(0, 250)
+hud_title.write("BREAKOUT", align="center", font=("", 24, "normal"))
 while True:
     pass
