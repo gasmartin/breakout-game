1064cfaf3515a0c323a12783756d57cbc70542e2
Vanessa Câmara <vdsc.snf19@uea.edu.br>
2019-10-05 15:06:46 -0400

    Adicionar botão play, scores, credits, exit

--- menu.py
+++ menu.py
@@ -1,7 +1,21 @@
 # criar tela
 from game_modules import objects
 screen = objects.create_screen("Breakout", 800, 600)
-hud_title = objects.create_hud(0, 250)
-hud_title.write("BREAKOUT", align="center", font=("", 24, "normal"))
+
+title_hud = objects.create_hud(0, 250)
+title_hud.write("BREAKOUT", align="center", font=("", 24, "normal"))
+
+play_hud = objects.create_hud(0, 70)
+play_hud.write("play", align="center", font=("", 18, "normal"))
+
+scores_hud = objects.create_hud(0, 30)
+scores_hud.write("scores", align="center", font=("", 18, "normal"))
+
+credits_hud = objects.create_hud(0, -10)
+credits_hud.write("credits", align="center", font=("", 18, "normal"))
+
+exit_hud = objects.create_hud(5, -50)
+exit_hud.write("exit", align="center", font=("", 18, "normal"))
+
 while True:
     pass
