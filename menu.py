209c7d8661ccc7a584abba1ad19e0bdd5d820daa
Vanessa Câmara <vdsc.snf19@uea.edu.br>
2019-10-06 23:35:15 -0400

    Adicionando lógica exit

--- menu.py
+++ menu.py
@@ -1,5 +1,8 @@
 # criar tela
 from game_modules import objects
+import turtle
+from time import sleep
+from game_modules import objects, sounds
 screen = objects.create_screen("Breakout", 800, 600)
 
 title_hud = objects.create_hud(0, 250)
@@ -14,8 +17,20 @@ scores_hud.write("scores", align="center", font=("", 18, "normal"))
 credits_hud = objects.create_hud(0, -10)
 credits_hud.write("credits", align="center", font=("", 18, "normal"))
 
-exit_hud = objects.create_hud(5, -50)
+exit_position_x = 0
+exit_position_y = -50
+exit_hud = objects.create_hud(exit_position_x, exit_position_y)
 exit_hud.write("exit", align="center", font=("", 18, "normal"))
 
+dx = 40
+dy = 40
+
+# para finalizar o jogo
+def exit_(x, y):
+    if(exit_position_x - dx <= x <= exit_position_x + dx and exit_position_y - dy <= y <= exit_position_y + dy):
+        screen.bye()
+
+screen.onscreenclick(exit_)
+
 while True:
-    pass
+    screen.update()
